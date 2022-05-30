ccd73f90eb96c4e3d8033f6bcf4693c1c8669f42
Mao <mauricio.vera.r7@gmail.com>
2022-05-30 12:21:25 -0400

    m2

---
+++ c1/intro_js/ejercicio.js
@@ -0,0 +1,20 @@
+// alert('hola js')
+// const variable fija que no podemos cambiar, creada por seguridad
+// let variable que podremos cambiar
+
+alert('Hola y bienvenidos')
+
+const nombre = prompt('cuál es tu nombre')
+alert('Hola ' + nombre)
+
+const edad = prompt('cuál es tu edad')
+console.log('tienes ' + edad + ' años')
+
+const saludo = 'Hola ' + nombre + ' de ' + edad + ' años'
+
+alert(saludo)
+
+const texto = prompt('ingrese un texto breve')
+caracteres = texto.length
+
+console.log('ingresó ' + caracteres + ' caracteres')
