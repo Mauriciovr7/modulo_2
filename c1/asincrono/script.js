cffb5c868fee2909ad0201e31370bc8dc195adde
Mao <mauricio.vera.r7@gmail.com>
2022-05-30 14:17:43 -0400

    snack

--- c1/asincrono/script.js
+++ c1/asincrono/script.js
@@ -7,13 +7,13 @@ console.log('tiene \n ' + `` + edad_dias + ' dias')
 // eje 2
 const edad_max = 83 // media
 const snack = prompt('cuál es tu snack favorito')
-const valor_snack = prompt('cuál es el valor de tu snack favorito')
+const valor_snack = parseInt(prompt('cuál es el valor de tu snack favorito'))
 const semanas = 56
 const snack_dia = 3
 const total_snack = (snack_dia * semanas) * edad_max
 const snack_faltan = total_snack - ((snack_dia * semanas) * edad)
 
-alert('Necesitarás ' + snack_faltan + ' snacks para que te alcancen hasta los ' + edad_max + ' años.')
+alert('Necesitarás ' + snack_faltan + ' ' + snack + ' para que te alcancen hasta los ' + edad_max + ' años.')
 
-alert('vas a gastar ' + valor_snack * snack_faltan + ' en snacks el resto de tu vida. ')
+alert('vas a gastar ' + valor_snack * snack_faltan + ' en ' + snack + ' el resto de tu vida. ')
 
